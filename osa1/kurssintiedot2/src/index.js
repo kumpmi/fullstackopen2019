3a64038844942ac180e9fe35b32129e1f5ab89cb
kumpmi <mikko.kumpulainen87@gmail.com>
2019-04-08 16:24:48 +0300

    Osa1 1.4: kurssitiedot step4
    1.4: kurssitiedot step4

--- osa1/kurssintiedot2/src/index.js
+++ osa1/kurssintiedot2/src/index.js
@@ -2,60 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    
     const course = 'Half Stack -sovelluskehitys'
-    const part1 = {
+    const parts = [
+      {
         name: 'Reactin perusteet',
         exercises: 10
-      }
-      const part2 = {
+      },
+      {
         name: 'Tiedonvälitys propseilla',
         exercises: 7
-      }
-      const part3 = {
+      },
+      {
         name: 'Komponenttien tila',
         exercises: 14
       }
+    ]
 
-
-    const Header = (props) => {
-        return (
-            <h1>
-                {course}
-            </h1>
-        )
-    }
-    const Content = (props) => {
+    const partsmap = parts.map((part) => 
+   <li> {part['name']} {part['exercises']} </li> 
+    );
+    //const total = parts.map((parts))
+             
+    const Total =  parts['0']['exercises'] + parts['1']['exercises'] + parts['2']['exercises']
+       
     
-        return (
-           <div>
-                <p>
-                   {part1['name']} {part1['exercises']}
-                </p>
-                <p>
-                     {part2['name']} {part2['exercises']}
-                </p>
-                <p>
-                    {part3['name']} {part3['exercises']}
-                </p>
-           </div>
-        )
-    }
-
-    const Total = (props) => {
-        return (
-            <div>
-                <p>
-                    Yhteensä : {part1['exercises'] + part2['exercises'] + part3['exercises']}
-                </p>
-            </div>
-        )
-    }
 
     return (
         <div>
-        <Header  />
-        <Content  />
-        <Total  />
+        <h1>{course}</h1>
+        <ul>
+            {partsmap}     
+        </ul>
+        <div>
+          Yhteensä: {Total}
+        </div>
+            
       </div>
     )
 }
